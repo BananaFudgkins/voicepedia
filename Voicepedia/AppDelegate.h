069c0bd005b2be10a59b91b5e6b934b6c2efa6b2
Michael Royzen <michael@mlabtechnologies.com>
2015-10-10 13:22:36 -0700

    Initial commit

---
+++ Voicepedia/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Voicepedia
+//
+//  Created by Michael Royzen on 10/10/15.
+//  Copyright © 2015 Michael Royzen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Voicepedia/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Voicepedia
+//
+//  Created by Michael Royzen on 10/10/15.
+//  Copyright © 2015 Michael Royzen. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
